1d6e654ca69ac56d4dc33ff4c2fd6a4420da357c
pikipity <wangze19910407@gmail.com>
2015-08-17 15:01:27 +0800

    Add result.txt for comparison results

--- Comparison.py
+++ Comparison.py
@@ -16,50 +16,58 @@ def test_correct(data,test_data):
         return True
     else:
         return False
+        
+def printline(line):
+    global f
+    print line
+    f.write(line+'\n')
 
 iteration_num=1000
+f=open('result.txt','w')
 # Comparison Results
 if test_correct(readdata("QuickSort.txt"),sorting_algorithm_1(readdata("QuickSort.txt"))):
-    print "Algorithm 1 correct"
+    printline("Algorithm 1 correct")
 else:
-    print "Algorithm 1 wrong"
-print "Algorithm 1 costs:"
-print "    time: %.3fs/%d iterations"%(timeit('sorting_algorithm_1(deepcopy(data))',\
+    printline("Algorithm 1 wrong")
+printline("Algorithm 1 costs:")
+printline("    %.3fs/%d iterations"%(timeit('sorting_algorithm_1(deepcopy(data))',\
                         setup='''from copy import deepcopy;from __main__ import readdata;from programming_question_1 import SortCount as sorting_algorithm_1;data=readdata("QuickSort.txt")''',\
-                        number=iteration_num),iteration_num)
+                        number=iteration_num),iteration_num))
                         
-print ""
+printline("")
 if test_correct(readdata("QuickSort.txt"),sorting_algorithm_2(readdata("QuickSort.txt"))):
-    print "Algorithm 2 correct"
+    printline("Algorithm 2 correct")
 else:
-    print "Algorithm 2 wrong"
-print "Algorithm 2 costs:"
-print "    time: %.3fs/%d iterations"%(timeit('sorting_algorithm_2(deepcopy(data))',\
+    printline("Algorithm 2 wrong")
+printline("Algorithm 2 costs:")
+printline("    %.3fs/%d iterations"%(timeit('sorting_algorithm_2(deepcopy(data))',\
                         setup='''from copy import deepcopy;from __main__ import readdata;from programming_question_2_problem_1 import QuickSort as sorting_algorithm_2;data=readdata("QuickSort.txt")''',\
-                        number=iteration_num),iteration_num)
+                        number=iteration_num),iteration_num))
                         
-print ""
+printline("")
 if test_correct(readdata("QuickSort.txt"),sorting_algorithm_3(readdata("QuickSort.txt"))):
-    print "Algorithm 3 correct"
+    printline("Algorithm 3 correct")
 else:
     print "Algorithm 3 wrong"
-print "Algorithm 3 costs:"
-print "    time: %.3fs/%d iterations"%(timeit('sorting_algorithm_3(deepcopy(data))',\
+printline("Algorithm 3 costs:")
+printline("    %.3fs/%d iterations"%(timeit('sorting_algorithm_3(deepcopy(data))',\
                         setup='''from copy import deepcopy;from __main__ import readdata;from programming_question_2_problem_2 import QuickSort as sorting_algorithm_3;data=readdata("QuickSort.txt")''',\
-                        number=iteration_num),iteration_num)
+                        number=iteration_num),iteration_num))
 
-print ""
+printline("")
 if test_correct(readdata("QuickSort.txt"),sorting_algorithm_4(readdata("QuickSort.txt"))):
-    print "Algorithm 4 correct"
+    printline("Algorithm 4 correct")
 else:
-    print "Algorithm 4 wrong"
-print "Algorithm 4 costs:"
-print "    time: %.3fs/%d iterations"%(timeit('sorting_algorithm_4(deepcopy(data))',\
+    printline("Algorithm 4 wrong")
+printline("Algorithm 4 costs:")
+printline("    %.3fs/%d iterations"%(timeit('sorting_algorithm_4(deepcopy(data))',\
                         setup='''from copy import deepcopy;from __main__ import readdata;from programming_question_2_problem_2 import QuickSort as sorting_algorithm_4;data=readdata("QuickSort.txt")''',\
-                        number=iteration_num),iteration_num)
+                        number=iteration_num),iteration_num))
 
-print ""
-print "'sorted' function costs:"
-print "    time: %.3fs/%d iterations"%(timeit('sorted(deepcopy(data))',\
+printline("")
+printline("'sorted' function costs:")
+printline("    %.3fs/%d iterations"%(timeit('sorted(deepcopy(data))',\
                         setup='''from copy import deepcopy;from __main__ import readdata;data=readdata("QuickSort.txt")''',\
-                        number=iteration_num),iteration_num)
+                        number=iteration_num),iteration_num))
+                        
+f.close()
